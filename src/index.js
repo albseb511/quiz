30a0c1c1abee905217d17acaf7fbf3017fa85280
albseb511 <albertsebastian511@gmail.com>
2020-12-20 14:30:29 +0530

    workbox, caching

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./routes/App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import "antd/dist/antd.css";
